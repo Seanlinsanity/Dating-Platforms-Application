a9862d173640754f89dab7dc9633bc311da3c7c0
Sean Lin <seanlin1016@gmail.com>
2019-02-02 17:59:05 +0800

    project initialization

---
+++ DatingPlatformApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DatingPlatformApplication
+//
+//  Created by SEAN on 2019/2/2.
+//  Copyright © 2019 SEAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
